09fcc5db76887a275e388a5ffc7ce09088482838
Burak Öner <info@burakoner.com>
2021-03-31 15:40:12 +0300

    Version 2.1.0

---
+++ CoinMarketCap.Samples/Program.cs
@@ -0,0 +1,16 @@
+﻿using CoinMarketCap.Pro.Parameters;
+using System;
+
+namespace CoinMarketCap.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cli = new CoinMarketCap.ProClient("******");
+            var map = cli.CryptocurrencyMap(listing_status: ListingStatus.active, 1, 2);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
